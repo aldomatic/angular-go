6a0b3194f2c7d189deb2ebd8f3f63eb5744fc0c9
Aldo Lugo <aldo@aldomatic.com>
2016-06-15 15:35:04 -0500

    Added comments

--- server/server.go
+++ server/server.go
@@ -5,6 +5,7 @@ import(
   "encoding/json"
 )
 
+// Person struct
 type Person struct{
     Name string `json:"name"`
     Title string `json:"title"`
@@ -12,10 +13,13 @@ type Person struct{
 }
 
 func main(){
+  // Assign a new instance of Person with grouped fields
   person := Person{"Brian", "Developer", 25}
+  // Encode the person variable to json bytes
   json, err := json.Marshal(person)
   if err != nil{
     fmt.Println("Error")
   }
+  // Convert json byte data into string and print
   fmt.Println(string(json))
 }
